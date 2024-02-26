ccef4e13e100b5fa494297f711107abf640dc23e
VinhTin-AQUA <tinhovinh@gmail.com>
2024-02-26 21:20:36 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-	{ path: '', redirectTo: 'ho-vinh-tin', pathMatch: 'full' },
+	{ path: '', redirectTo: '/ho-vinh-tin', pathMatch: 'full' },
 	{ path: 'ho-vinh-tin', component: HomeComponent, title: 'Hồ Vĩnh Tín' },
 ];
