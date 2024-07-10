a34213c77b3a9b41a1d5e8f7e4d75ad25abe3511
VinhTin-AQUA <tinhovinh@gmail.com>
2024-07-10 20:59:48 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ThemeService } from './themes/theme.service';
 import { CommonModule } from '@angular/common';
+import { ThemeService } from './theme_modes/theme.service';
 
 @Component({
 	selector: 'app-root',
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { ThemeService } from '../themes/theme.service';
+import { ThemeService } from '../theme_modes/theme.service';
 
 @Component({
 	selector: 'app-home',
