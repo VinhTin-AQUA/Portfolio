19d7d4b87cc3262e8f048cbd325cc1402d8bf0c7
VinhTin-AQUA <tinhovinh@gmail.com>
2025-02-20 13:44:59 +0700

    add sticky navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
 	
 	{ path: 'home', component: HomeComponent, title: 'Home' },
 	{ path: 'experience', component: ExperienceComponent, title: 'Experience' },
+	{ path: 'projects', component: ProjectsComponent, title: 'Projects' },
     { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
