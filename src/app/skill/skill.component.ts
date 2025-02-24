aa2f599e3ac1a768e3177a87076162a60548fc6c
VinhTin-AQUA <tinhovinh@gmail.com>
2025-02-24 13:18:02 +0700

    add skills

--- src/app/skill/skill.component.ts
+++ src/app/skill/skill.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { skills } from './sills.data';
+import { SvgComponent } from '../components/svgs/svg/svg.component';
 
 @Component({
-  selector: 'app-skill',
-  imports: [],
-  templateUrl: './skill.component.html',
-  styleUrl: './skill.component.scss'
+	selector: 'app-skill',
+	imports: [SvgComponent],
+	templateUrl: './skill.component.html',
+	styleUrl: './skill.component.scss',
 })
 export class SkillComponent {
-
+	skills: any = skills;
 }
